6ff941e69d3b4dbdc245f99ea757a936688607f7
jpozarycki <jakub.pozarycki@gmail.com>
2019-01-27 18:31:58 +0100

    Adding Index page and controller. Closes #8

--- pet-clinic-web/src/main/java/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package com.pozarycki.sfgpetclinic;
+package sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pet-clinic-web/src/test/java/com/pozarycki/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/pozarycki/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import sfgpetclinic.SfgPetClinicApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest (classes = SfgPetClinicApplication.class)
