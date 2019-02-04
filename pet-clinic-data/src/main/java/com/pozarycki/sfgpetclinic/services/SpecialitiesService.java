b7ac4c73744e132c0c9be0880bf509f088affdad
jpozarycki <jakub.pozarycki@gmail.com>
2019-02-04 21:18:23 +0100

    Added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/pozarycki/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.pozarycki.sfgpetclinic.services;
+
+import com.pozarycki.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
--- pet-clinic-data/src/main/java/com/pozarycki/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/pozarycki/sfgpetclinic/services/map/VetMapService.java
@@ -5,22 +5,23 @@ import com.pozarycki.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+
 @Service
 
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
-        @Override
-        public Set<Vet> findAll() {
+    @Override
+    public Set<Vet> findAll() {
         return super.findAll();
     }
 
-        @Override
-        public void deleteById(Long id) {
+    @Override
+    public void deleteById(Long id) {
         super.deleteById(id);
     }
 
-        @Override
-        public Vet findById(Long id) {
+    @Override
+    public Vet findById(Long id) {
         return super.findById(id);
     }
 
