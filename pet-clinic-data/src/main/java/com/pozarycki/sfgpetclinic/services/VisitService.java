7ee5c92daca71266da4c3ebff69dd946f32353ce
jpozarycki <jakub.pozarycki@gmail.com>
2019-02-09 10:54:36 +0100

    Implemented VisitService and VisitMapService. Closes #46

---
+++ pet-clinic-data/src/main/java/com/pozarycki/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,8 @@
+package com.pozarycki.sfgpetclinic.services;
+
+import com.pozarycki.sfgpetclinic.model.Visit;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface VisitService extends CrudService<Visit, Long> {
+}
