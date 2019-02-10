3edb0f23b8612c9b8f7edd4ef14599d96d64c8f5
jpozarycki <jakub.pozarycki@gmail.com>
2019-02-10 11:28:59 +0100

    Added JUnit 5. Closes #4

--- pet-clinic-web/src/test/java/com/pozarycki/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/pozarycki/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,12 +1,12 @@
 package com.pozarycki.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class SfgPetClinicApplicationTests {
 
     @Test
